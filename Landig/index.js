a7e3627cf826e51cf34849a3f647921beeb4667f
Daniel Solarte <royersolarte22@gmail.com>
2024-06-04 21:44:49 -0500

    landing

--- Landig/index.js
+++ Landig/index.js
@@ -5,17 +5,36 @@ root.render(
         <Header />
         <Main />
         <Section2 />
+        <GamesContainer />
+        <Fottee />
     </>
 );
 
+function handleScroll() {
+    let navi = document.getElementById("navi");
+    if (window.scrollY > 0) {
+        navi.classList.add('bg-GrisD');
+    } else {
+        navi.classList.remove('bg-GrisD');
+    }
+}
+
 function Header() {
+    React.useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        // Limpieza del evento al desmontar el componente
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
-        <nav className="nav">
-            <h1 className="nombre">Daniel</h1>
+        <nav id="navi" className="nav">
+            <h1 className="nombre">HALO</h1>
             <ul className="lista1">
-                <li className=''>Inicio</li>
-                <li>Nosotros</li>
-                <li>Contacto</li>
+                <li><a href="#inicio">Inicio</a></li>
+                <li><a href="#nosotros">Nosotros</a></li>
+                <li><a href="#games">Contacto</a></li>
             </ul>
         </nav>
     )
@@ -23,10 +42,10 @@ function Header() {
 
 function Main() {
     return (
-        <section className="fondo">
+        <section id="inicio" className="fondo">
             <div className="container">
-                <img className="perfil" src="https://www.pngall.com/wp-content/uploads/12/Avatar-Profile-PNG.png" alt="" />
-                <h1>Daniel Solarte</h1>
+                <img className="perfil" src="https://b.thumbs.redditmedia.com/aEl0W9rk2ntR2NKoBkuk-F2n_iAfspGapXsHIrbi2OE.png" alt="" />
+                <h1>HALO</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, voluptatibus!</p>
             </div>
             <h1 className="bienvenidos">¡Bienvenidos!</h1>
@@ -36,19 +55,65 @@ function Main() {
 
 function Section2() {
     return (
-        <div className="container2">
+        <div id="nosotros" className="container2">
             <div className="div2">
                 <h1 >Sobre Nosotros</h1>
                 <section className="cont">
                     <article className="der">
-                        <img className="perfil" src="https://cdn.atomix.vg/wp-content/uploads/2010/04/316.jpg" alt="" />
+                        <img className="perfil" src="https://preview.redd.it/mean-green-fighting-machine-my-master-chief-cosplay-v0-srv2z1a1ih0b1.jpg?width=640&crop=smart&auto=webp&s=99706156f3cd4e8fd89a76645d631790c509afe5" alt="" />
                     </article>
 
                     <section className="isq">
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium cumque voluptatem enim sint? Porro omnis reiciendis temporibus ullam laboriosam in at voluptate delectus nostrum obcaecati! Impedit ratione velit perferendis obcaecati?</p>
+                        <p>Halo es una franquicia de videojuegos de ciencia ficción creada y desarrollada por Bungie Studios hasta Halo Reach, y gestionada ahora por 343 Industries, propiedad de Xbox Game Studios. La serie se centra en una guerra interestelar entre la humanidad y una alianza teocrática de alienígenas conocidos como Covenant.</p>
                     </section>
                 </section>
             </div>
         </div>
+    )
+}
+
+function GamesContainer() {
+    return (
+        <section id="games" className="contenido">
+            <div className="targeta">
+                <img src="https://upload.wikimedia.org/wikipedia/en/9/9c/HaloCEA_boxart.png" alt="" />
+                <h3>Halo CE</h3>
+                <p></p>
+            </div>
+            <div className="targeta">
+                <img src="https://upload.wikimedia.org/wikipedia/en/9/92/Halo2-cover.png" alt="" />
+                <h3>Halo 2</h3>
+                <p></p>
+            </div>
+            <div className="targeta">
+                <img src="https://upload.wikimedia.org/wikipedia/en/b/b4/Halo_3_final_boxshot.JPG" alt="" />
+                <h3>Halo 3</h3>
+                <p></p>
+            </div>
+            <div className="targeta">
+                <img src="https://upload.wikimedia.org/wikipedia/en/9/92/Halo_4_box_artwork.png" alt="" />
+                <h3>Halo 4</h3>
+                <p></p>
+            </div>
+            <div className="targeta">
+                <img src="https://upload.wikimedia.org/wikipedia/en/6/68/Halo_Guardians.png" alt="" />
+                <h3>Halo 5</h3>
+                <p></p>
+            </div>
+            <div className="targeta">
+                <img src="https://upload.wikimedia.org/wikipedia/en/5/5c/Halo-_Reach_box_art.png" alt="" />
+                <h3>Halo Reach</h3>
+                <p></p>
+            </div>
+        </section>
+
+    )
+}
+
+function Fottee() {
+    return (
+        <footer className="fott">
+            <h1>HALO</h1>
+        </footer>
     )
 }
--- Practicas-React/src/App.jsx
+++
@@ -1,5 +0,0 @@
-import WeatherApp from "./components/WeaterApp"
-
-export default function App(){
-    return <WeatherApp />
-}
